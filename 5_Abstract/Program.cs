59bb207452253295345e6b51f074834bff857028
Sreemathy Myhre <140131637+SreemathyArunagiri@users.noreply.github.com>
2025-02-12 11:59:40 -0600

    Abstract
    Topics Covered....
             * Abstract Classes, Abstract Methods
             * Base Classes, Is, As, Casting and
             * more about Polymorphism

---
+++ 5_Abstract/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStarttoFinish._5_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We can store our shapes in
+            // a Shape array as long as it 
+            // contains subclasses of Shape
+            Shape[] shapes = {new Circle(5),
+            new Rectangle(4,5)};
+            
+            foreach (Shape s in shapes)
+            {
+                // Call the overidden method
+                s.GetInfo();
+
+                Console.WriteLine("{0} Area : {1:f2}",
+                s.Name, s.Area());
+
+                Circle testCirc = s as Circle;
+                if (testCirc == null)
+                {
+                    Console.WriteLine("This isn't a Circle");
+                }
+                                
+                if (s is Circle)
+                {
+                    Console.WriteLine("This isn't a Rectangle");
+                }
+
+
+                Console.WriteLine();
+            }
+
+            // You can store any class as a base
+            // class and call the subclass methods
+            // even if they don't exist in the base
+            // class by casting
+            object circ1 = new Circle(4);
+
+            Circle circ2 = (Circle)circ1;
+            Console.WriteLine("The {0} Area is {1:f2}",
+                circ2.Name, circ2.Area());
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 5_Abstract/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CSharpStarttoFinish._5_Abstract
+{
+    class Rectangle : Shape
+    {
+
+        public double Length { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle(double length,
+            double width)
+        {
+            Name = "Rectangle";
+            Length = length;
+            Width = width;
+        }
+
+        public override double Area()
+        {
+            return Length * Width;
+        }
+
+        public override void GetInfo()
+        {
+            // Execute the base version
+            base.GetInfo();
+            Console.WriteLine($"It has a Length of {Length} and Width of {Width}");
+        }
+    }
+}
